24f89e356d47fda5c73bee5c4b0b4e48f38fa821
Zach Haitz <zach@indie.app>
2022-12-16 15:27:08 -0500

    Support globs for matching code ownership

--- tests/format/summary/getSummary.test.ts
+++ tests/format/summary/getSummary.test.ts
@@ -16,17 +16,43 @@ describe('getSummary', () => {
     });
 
     it('should calculate summary with codeowners', () => {
-        const counter = jest.fn(() => 1);
+        const counter = jest.fn(() => 1); // each file has only one 1 line and it's covered
 
         expect(
             getSummary(coverageMap, counter, counter, 'Title', {
                 team: 'testing',
-                paths: ['/jest/examples/typescript/CheckboxWithLabel.tsx'],
+                paths: ['/jest/examples/typescript/sum*'],
+            })
+        ).toStrictEqual({
+            title: 'Title',
+            total: 2,
+            covered: 2,
+            percentage: 100,
+            team: 'testing',
+        });
+
+        expect(
+            getSummary(coverageMap, counter, counter, 'Title', {
+                team: 'testing',
+                paths: ['/jest/examples/typescript/sum.js'],
             })
         ).toStrictEqual({
             title: 'Title',
             total: 1,
             covered: 1,
+            percentage: 100,
+            team: 'testing',
+        });
+
+        expect(
+            getSummary(coverageMap, counter, counter, 'Title', {
+                team: 'testing',
+                paths: ['/jest/examples/typescript/*'],
+            })
+        ).toStrictEqual({
+            title: 'Title',
+            total: 6,
+            covered: 6,
             percentage: 100,
             team: 'testing',
         });
